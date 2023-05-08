3b217edfd2f9b7165622a39b126eff061950d0d6
Lexei57 <mnzllexei@gmail.com>
2023-05-08 16:32:14 +0300

    Created calendar

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as moment from 'moment';
+import {DateService} from '../../services/date.service';
+
+interface IDay {
+  value: moment.Moment
+  active: boolean
+  disabled: boolean
+  selected: boolean
+}
+
+interface IWeek {
+  days: IDay[]
+}
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
 
+  calendar: IWeek[]
+
+  constructor(private dateService: DateService) {
+  }
+
+  ngOnInit(): void {
+    this.dateService.date.subscribe(date => this.generate(date))
+  }
+
+  generate(now: moment.Moment) {
+    const startDay = now.clone().startOf('month').startOf('week')
+    const endDay = now.clone().endOf('month').endOf('week')
+
+    const date = startDay.clone().subtract(1, 'day')
+
+    const calendar = []
+
+    while (date.isBefore(endDay, 'day')) {
+      calendar.push({
+        days: Array(7)
+          .fill(0)
+          .map(() => {
+            const value = date.add(1, 'day').clone()
+            const active = moment().isSame(value, 'date')
+            const disabled = !now.isSame(value, 'month')
+            const selected = now.isSame(value, 'date')
+
+            return {
+              value, active, disabled, selected
+            }
+          })
+      })
+    }
+    this.calendar = calendar
+  }
+
+  selectDay(day: moment.Moment): void {
+    this.dateService.changeDay(day)
+  }
 }
